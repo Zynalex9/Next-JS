4869d377ea73a226b4291ff1cc743795aba87636
Zynalex9 <alzn952@gmail.com>
2024-08-28 09:53:57 +0500

    Not found page

---
+++ firstapp/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <h2>Page you're trying to access doesn't exist</h2>
+    </div>
+  )
+}
+
+export default PageNotFound
--- firstapp/app/portfolio/[id]/page.jsx
+++ firstapp/app/portfolio/[id]/page.jsx
@@ -1,7 +1,8 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 const Page = ({params}) => {
-
+if (params.id > 10) notFound()
   return <div>This is id: {params.id} </div>;
 };
 
