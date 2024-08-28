807898abae0f0a4d476694875dcf76f727909f07
Zynalex9 <alzn952@gmail.com>
2024-08-28 13:01:08 +0500

    Zod

--- firstapp/app/api/users/[id]/route.jsx
+++ firstapp/app/api/users/[id]/route.jsx
@@ -1,20 +1,24 @@
 import { NextResponse } from "next/server";
-
+import { schema } from "../schema";
 export function GET(request, { params }) {
   return NextResponse.json({ id: params.id, name: "Zain" });
 }
 export async function PUT(request, { params }) {
   const body = await request.json();
-  if (!body.name) {
+  const validation = schema.safeParse(body)
+  if (!validation.success) {
     return NextResponse.json(
-      { error: "Name is required to update user" },
+      { error: validation.error.errors },
       { status: 400 }
     );
   }
   if (params.id > 10) {
     return NextResponse.json({ error: "Enter a valid user" }, { status: 404 });
   }
-  return NextResponse.json({ id: params.id, name: body.name });
+  return NextResponse.json(
+    { id: params.id, name: body.name, email: body.email, age: body.age },
+    { status: 200 }
+  );
 }
 
 export function DELETE(request, { params }) {
--- firstapp/app/api/users/route.jsx
+++ firstapp/app/api/users/route.jsx
@@ -1,23 +1,31 @@
 import { NextResponse } from "next/server";
-
+import { schema } from "./schema";
 export function GET(request) {
   return NextResponse.json([
     {
       id: 1,
       name: "John",
+      email: "abc@gmail.com",
+      age: 30,
     },
     {
       id: 2,
       name: "Micheal",
+      email: "abc@h.com",
+      age: 20,
     },
   ]);
 }
 export async function POST(request) {
   const body = await request.json();
-  if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(
+      { error: validation.error.errors },
+      { status: 400 }
+    );
   return NextResponse.json(
-    { id: Date.now(), name: body.name },
-    { status: 201 }
+    { id: Date.now(), name: body.name, email: body.email, age: body.age },
+    { status: 200 }
   );
 }
