9db8193282b939d2494942ef96c3a13b5f24ea13
Zynalex9 <alzn952@gmail.com>
2024-08-28 09:19:53 +0500

    useRouter

--- firstapp/app/login/page.jsx
+++ firstapp/app/login/page.jsx
@@ -1,11 +1,20 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const page = () => {
+  const router = useRouter();
   return (
     <div>
       <h1>Login</h1>
-      <Link href="/">Home</Link>
+      <button
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        Back to Home
+      </button>
     </div>
   );
 };
