265b9cef302e19e6e1ca726b45a940f1411fae18
Zynalex9 <alzn952@gmail.com>
2024-08-29 10:02:15 +0500

    PUT and DELETE request

--- firstapp/app/api/users/[id]/route.jsx
+++ firstapp/app/api/users/[id]/route.jsx
@@ -1,21 +1,22 @@
 import { NextResponse } from "next/server";
 import { schema } from "../schema";
-import prisma from '@/prisma/client';
+import prisma from "@/prisma/client";
 
 export async function GET(request, { params }) {
-  const userId = parseInt(params.id, 10); 
+  const userId = parseInt(params.id, 10);
   if (isNaN(userId)) {
-    return NextResponse.json({ error: 'Invalid user ID' }, { status: 400 });
+    return NextResponse.json({ error: "Invalid user ID" }, { status: 400 });
   }
   const user = await prisma.user.findUnique({
     where: { id: userId },
   });
   if (!user) {
-    return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
   return NextResponse.json(user);
 }
 export async function PUT(request, { params }) {
+  const userID = parseInt(params.id, 10);
   const body = await request.json();
   const validation = schema.safeParse(body);
   if (!validation.success) {
@@ -24,18 +25,32 @@ export async function PUT(request, { params }) {
       { status: 400 }
     );
   }
-  if (params.id > 10) {
+  const user = await prisma.user.findUnique({
+    where: { id: userID },
+  });
+  if (!user) {
     return NextResponse.json({ error: "Enter a valid user" }, { status: 404 });
   }
-  return NextResponse.json(
-    { id: params.id, name: body.name, email: body.email, age: body.age },
-    { status: 200 }
-  );
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+  return NextResponse.json(updatedUser, { status: 200 });
 }
 
-export function DELETE(request, { params }) {
-  if (params.id > 10) {
+export async function DELETE(request, { params }) {
+  const userID = parseInt(params.id, 10);
+  const user = await prisma.user.findUnique({
+    where: { id: userID },
+  });
+  if (!user) {
     return NextResponse.json({ error: "Inavalid user" }, { status: 400 });
   }
-  return NextResponse.json({});
+  await prisma.user.delete({
+    where: { id: user.id },
+  });
+  return NextResponse.json(`${user.name} is deleted`);
 }
