8aeba15cee5061d95abe9b3c27161171eb524eab
Zynalex9 <alzn952@gmail.com>
2024-08-30 15:11:08 +0500

    Login with NextAuth

--- firstapp/app/api/auth/[...nextauth]/route.jsx
+++ firstapp/app/api/auth/[...nextauth]/route.jsx
@@ -1,11 +1,40 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/prisma/client";
+import bcrypt from "bcrypt";
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter your email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter your password",
+        },
+      },
+      async authorize(credentials, req) {
+        if (!credentials.email || !credentials.password) return null;
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if(!user) return null
+        const passMatch = await bcrypt.compare(
+          credentials.password,
+          user.hashPassowrd
+        );
+        return passMatch ? user : null;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
